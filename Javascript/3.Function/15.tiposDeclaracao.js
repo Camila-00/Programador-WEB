3c863abbc30ca95c3a1aa4a599f4b7388f84fc33
Camila Isabela <53484905+Camila-Isabela@users.noreply.github.com>
2020-06-26 16:32:26 -0300

    🖇closure - class x constructor x factory

---
+++ Javascript/3.Function/15.tiposDeclaracao.js
@@ -0,0 +1,23 @@
+console.log(soma(4, 3));
+
+//Function Declaration (+ utilizada e flexível)
+
+function soma(x, y) {
+    return x + y
+};
+
+//Function Expression  (+ utilizada)
+
+const subtracao = function(x, y) {
+    return x - y
+};
+
+console.log(subtracao(4, 3));
+
+//Function Named Expression (- utilizada)
+
+const multiplicacao = function multiplicacao(x, y) {
+    return x * y
+};
+
+console.log(multiplicacao(4, 3));
